f964e6d1957b42dad01aca94b9191bdf5236c97a
unknown <ilantc@C403-2.iem.technion.ac.il>
2014-12-31 19:15:14 +0200

    adding skeleton and responsibilities

---
+++ src/model.py
@@ -0,0 +1,37 @@
+
+
+class mstModel:
+    
+    featureSet = []
+    w = []
+    featureDict = {'pWord': {}, 'pPos' : {}, 'cWord': {}, 'cPos': {}}
+    
+    
+    def __init__(self):
+        return
+    
+    # TODO - Ilan
+    def readFile(self):
+        return
+    
+    # TODO - Ilan
+    def buildFeatures(self):
+        return
+    
+    # TODO - Liora
+    def train(self):
+        return
+    
+    # TODO - Liora
+    def perceptron(self):
+        return
+    
+    # TODO - Ilan
+    def ciuLiuEdmonds(self):
+        return
+    
+    # TODO - Ilan
+    def test(self):
+        return
+    
+    
